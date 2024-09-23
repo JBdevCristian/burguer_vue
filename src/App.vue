a5d5c02c76a433a24e82f489828766a891525787
JBdevCristian <jbcristian78@gmail.com>
2024-09-22 23:51:29 -0300

    Adicionando Navbar/Footer e configurando router

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <Navbar />
   <router-view/>
+  <Footer />
 </template>
+
+<script>
+import Navbar from './components/NavBar.vue'
+import Footer from './components/Footer.vue'
+
+
+  export default {
+    components: {
+      Navbar,
+      Footer
+    }
+  }
+
+</script>
 
 <style>
 
