9af3fe4a3365eb4bbcbea004ce1c3e6241614941
JBdevCristian <jbcristian78@gmail.com>
2024-09-26 13:37:52 -0300

    Finalizando navbar/footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,22 @@
 <template>
-    <h1>Footer</h1>
+    <footer id="footer">
+        <p>Make your burguer &copy; 2024</p>
+    </footer>
 </template>
 
 <script>
 
 </script>
 
-<style>
-
+<style escoped>
+    #footer {
+        height: 345px;
+        background-color: #222;
+        border-top: 4px solid #111;
+        color: #fcba03;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+    }
 </style>
